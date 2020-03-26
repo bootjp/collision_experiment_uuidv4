873f699df5bb5d5275dde49b930c85d7796876f5
bootjp / yoshiaki.ueda <oh@bootjp.me>
2020-03-26 17:25:08 +0900

    change output uuid to atomic counter

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/bootjp/collision_experiment_uuidv4
 go 1.12
 
 require (
-	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/deckarep/golang-set v1.7.1
 	github.com/google/btree v1.0.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/satori/go.uuid v1.2.0
--- run.go
+++ run.go
@@ -3,19 +3,20 @@ package main
 import (
 	"fmt"
 	mapset "github.com/deckarep/golang-set"
+	"github.com/satori/go.uuid"
 	"os"
 	"runtime"
-
-	"github.com/satori/go.uuid"
+	"sync/atomic"
 )
 
 
 func main() {
+	var ops uint64
 	max := runtime.NumCPU() * 2
 	store := mapset.NewSet()
 
 	for i := 0; i < max; i++ {
-		go run(store)
+		go run(store, &ops)
 	}
 
 	c := make(chan byte)
@@ -23,16 +24,16 @@ func main() {
 	fmt.Println(wait)
 }
 
-func run(s mapset.Set) {
+func run(s mapset.Set, cnt *uint64) {
 	for {
-
 		u1 := uuid.NewV4()
-
 		if s.Contains(u1) {
-			println("collision uuid!")
+			fmt.Println("collision uuid!")
+			fmt.Printf("genereted %d uuid\n", cnt)
 			os.Exit(1)
 		}
-		fmt.Println(u1.String())
+		atomic.AddUint64(cnt, 1)
+		fmt.Printf("%d\n", *cnt)
 		s.Add(u1)
 	}
 }
